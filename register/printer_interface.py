fcfd335d1e200977b97d2e222a07047c6d909196
uas <uas@GCS-5>
2015-12-08 12:20:20 +1030

    printer: fixed typo and PEP-8 compliance

--- register/printer_interface.py
+++ register/printer_interface.py
@@ -1,34 +1,33 @@
-
-
 class Printer(object):
     '''generic printer object wrapper'''
+
     def __init__(self, settings):
-        if type(settings) is str: #support for existing settings
+        if type(settings) is str:  # support for existing settings
             self.spool = settings
             self.printer_driver = 'orthosie'
         else:
-            #the format should be {'spool':'x', 'driver':'y', 'interface':'z'}
+            # the format should be {'spool':'x', 'driver':'y', 'interface':'z'}
             self.spool = settings['spool']
             self.printer_driver = settings['driver']
             self.interface = settings['interface']
-            
-        
+
         if self.printer_driver == 'orthosie':
-            self._printer =  OrthosiePrinter(self.spool)
-           
+            self._printer = OrthosiePrinter(self.spool)
+
         elif self.printer_driver == 'escpos':
-            self._printer =  ESCPOSPrinter(self.spool, self.interface)
-            
+            self._printer = ESCPOSPrinter(self.spool, self.interface)
+
         else:
             raise PrinterTypeNotSupported(
                 'Printer driver "' + self.printer_driver + '" not supported.'
-                )
+            )
+
     def open(self):
         try:
             self._printer.open()
         except AttributeError as e:
             print e
-    
+
     def close(self):
         try:
             self._printer.close()
@@ -37,44 +36,45 @@ class Printer(object):
 
     def print_line(self, line):
         self._printer.print_line(line)
-        
+
     def print_image(self, image_path):
         try:
             self._printer.print_image(image_path)
         except AttributeError as e:
             print e
-            
+
     def print_qr(self, text):
         try:
             self._printer.print_qr(text)
         except AttributeError as e:
             print e
-    
+
     def print_barcode(self, **kwargs):
         try:
             self._printer.print_barcode(**kwargs)
         except AttributeError as e:
             print e
-    
+
     def cut(self, **kwargs):
         self._printer.cut(**kwargs)
 
     def kick_drawer(self, **kwargs):
         self._printer.kick_drawer(**kwargs)
-    
+
     def set(self, **kwargs):
         try:
             self._printer.set(**kwargs)
         except AttributeError as e:
             print e
 
-        
+
 class OrthosiePrinter(object):
     '''Default orthosie printer'''
+
     def __init__(self, spool):
         self.spool = spool
         self._printer = None
-        
+
     def open(self):
         try:
             self._printer = open(self.spool, 'w')
@@ -102,6 +102,7 @@ class OrthosiePrinter(object):
 
 class ESCPOSPrinter(object):
     '''Epson printer'''
+
     def __init__(self, spool, interface):
         from escpos import printer as escpos_printer
         self.spool = spool
@@ -115,43 +116,41 @@ class ESCPOSPrinter(object):
         elif self.interface == 'file':
             self._printer = escpos_printer.File(self.spool)
         else:
-            #TODO: handle unsupported printer types
+            # TODO: handle unsupported printer types
             pass
-            
+
     def print_line(self, line):
-        self._printer.text(line+"\n")
+        self._printer.text(line + "\n")
 
     def cut(self, **kwargs):
         self._printer.cut(**kwargs)
-        
+
     def set(self, **kwargs):
         self._printer.set(**kwargs)
 
     def kick_drawer(self, **kwargs):
-        '''EPSON draw kick - supported pins are 2 & 5'''
+        '''EPSON drawer kick - supported pins are 2 & 5'''
         if 'pin' in kwargs.keys():
             pin = kwargs['pin']
         else:
-            pin = 2 #default to 2
+            pin = 2  # default to 2
         self._printer.cashdraw(pin)
-        self._printer.cashdraw(pin)#required by some kickers
+        self._printer.cashdraw(pin)  # required by some kickers
 
     def print_qr(self, text):
         self._printer.qr(text)
-    
+
     def print_image(self, image_path):
         self._printer.image(image_path)
-    
+
     def print_barcode(self, **kwargs):
         expected = ['code', 'bc', 'width', 'height', 'pos', 'font']
         for key in expected:
-            if not key in kwargs:
+            if key not in kwargs:
                 raise Exception("All arguments rquired")
-        
+
         self._printer.barcode(kwargs['code'], kwargs['bc'], kwargs['width'],
-                               kwargs['height'], kwargs['pos'], kwargs['font'])
-
-
+                              kwargs['height'], kwargs['pos'], kwargs['font'])
 
 
 class PrinterNotFound(Exception):
@@ -161,7 +160,8 @@ class PrinterNotFound(Exception):
 
     def __str__(self):
         return self.value
-    
+
+
 class PrinterTypeNotSupported(Exception):
 
     def __init__(self, value):
@@ -171,15 +171,17 @@ class PrinterTypeNotSupported(Exception):
         return self.value
 
 if __name__ == '__main__':
-    printer = Printer({'spool':'/dev/null', 'driver':'orthosie', 'interface':''})#will use the orthosie driver
-    printer = Printer('/dev/null')#will use the orthosie driver
-    printer = Printer({'spool':'192.168.192.168', 'driver':'escpos', 'interface':'network'})
-    kwargs = {'font':'A','align':'CENTER'}
+    # will use the orthosie driver
+    printer = Printer(
+        {'spool': '/dev/null', 'driver': 'orthosie', 'interface': ''})
+    printer = Printer('/dev/null')  # will use the orthosie driver
+    printer = Printer({'spool': '192.168.192.168',
+                       'driver': 'escpos', 'interface': 'network'})
+    kwargs = {'font': 'A', 'align': 'CENTER'}
     printer.set(**kwargs)
     printer.print_line('Hello World!')
-    kwargs = {'mode':'PART'}
-    printer.cut(**kwargs) #this is a part cut
-    kwargs = {'pin':2}
-    printer.kick_drawer(**kwargs) #draw kick using pin 2
-    printer.cut() #this is a full cut
-    
+    kwargs = {'mode': 'PART'}
+    printer.cut(**kwargs)  # this is a part cut
+    kwargs = {'pin': 2}
+    printer.kick_drawer(**kwargs)  # draw kick using pin 2
+    printer.cut()  # this is a full cut
