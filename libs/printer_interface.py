3c2b1373c4c80c518651c764bcc62ec31229b46c
uas <uas@GCS-5>
2015-12-09 11:17:20 +1030

    Printer: sanitize user input and handle errors

--- libs/printer_interface.py
+++ libs/printer_interface.py
@@ -3,36 +3,54 @@ class Printer(object):
 
     def __init__(self, settings):
         if type(settings) is str:  # support for existing settings
-            self.spool = settings
-            self.printer_driver = 'orthosie'
-        else:
-            # the format should be {'spool':'x', 'driver':'y', 'interface':'z'}
-            self.spool = settings['spool']
-            self.printer_driver = settings['driver']
-            self.interface = settings['interface']
-
-        if self.printer_driver == 'orthosie':
+            self.spool = str(settings)  # spool is case sensitive
+            self.printer_driver = 'ORTHOSIE'
             self._printer = OrthosiePrinter(self.spool)
-
-        elif self.printer_driver == 'escpos':
-            self._printer = ESCPOSPrinter(self.spool, self.interface)
-
         else:
-            raise PrinterTypeNotSupported(
-                'Printer driver "' + self.printer_driver + '" not supported.'
-            )
+            if type(settings) is dict:
+                # the format should be {'spool':'x', 'driver':'y', 'interface':'z'}
+                self.spool = str(settings['spool'])  # spool is case sensitive
+                self.printer_driver = str(settings['driver']).upper()
+                self.interface = str(settings['interface']).upper()
+
+                if self.printer_driver == 'ORTHOSIE':
+                    self._printer = OrthosiePrinter(self.spool)
+
+                elif self.printer_driver == 'ESCPOS':
+                    self._printer = ESCPOSPrinter(self.spool, self.interface)
+
+                else:
+                    raise PrinterTypeNotSupported(
+                        'Printer driver "' + self.printer_driver + '" not supported.'
+                    )
+            else:
+                # the settings are not in a dict format
+                raise PrinerSettingsError(
+                    'Printer settings format not supported.'
+                )
+
+    # methods supported by all printer drivers
+    def cut(self, **kwargs):
+        self._printer.cut(**kwargs)
+
+    def kick_drawer(self, **kwargs):
+        self._printer.kick_drawer(**kwargs)
 
     def open(self):
         try:
             self._printer.open()
-        except AttributeError as e:
-            print(e)
+        except AttributeError:
+            raise PrinterAttributeError(
+                '"open" is unsupported by printer'
+            )
 
     def close(self):
         try:
             self._printer.close()
-        except AttributeError as e:
-            print(e)
+        except AttributeError:
+            raise PrinterAttributeError(
+                '"close" is unsupported by printer'
+            )
 
     def print_line(self, line):
         self._printer.print_line(line)
@@ -40,32 +58,34 @@ class Printer(object):
     def print_image(self, image_path):
         try:
             self._printer.print_image(image_path)
-        except AttributeError as e:
-            print(e)
+        except AttributeError:
+            raise PrinterAttributeError(
+                '"print_image" is unsupported by printer'
+            )
 
     def print_qr(self, text):
         try:
             self._printer.print_qr(text)
-        except AttributeError as e:
-            print(e)
+        except AttributeError:
+            raise PrinterAttributeError(
+                '"print_qr" is unsupported by printer'
+            )
 
     def print_barcode(self, **kwargs):
         try:
             self._printer.print_barcode(**kwargs)
-        except AttributeError as e:
-            print(e)
-
-    def cut(self, **kwargs):
-        self._printer.cut(**kwargs)
-
-    def kick_drawer(self, **kwargs):
-        self._printer.kick_drawer(**kwargs)
+        except AttributeError:
+            raise PrinterAttributeError(
+                '"print_barcode" is unsupported by printer'
+            )
 
     def set(self, **kwargs):
         try:
             self._printer.set(**kwargs)
-        except AttributeError as e:
-            print(e)
+        except AttributeError:
+            raise PrinterAttributeError(
+                '"set" is unsupported by printer'
+            )
 
 
 class OrthosiePrinter(object):
@@ -114,17 +134,52 @@ class ESCPOSPrinter(object):
         from escpos import printer as escpos_printer
         self.spool = spool
         self.interface = interface
-        if self.interface == 'usb':
-            self._printer = escpos_printer.Usb(self.spool)
-        elif self.interface == 'serial':
-            self._printer = escpos_printer.Serial(self.spool)
-        elif self.interface == 'network':
-            self._printer = escpos_printer.Network(self.spool)
-        elif self.interface == 'file':
-            self._printer = escpos_printer.File(self.spool)
+
+        # This is kind of a hacky way to make this work in Python 2.7.
+        # IOError can be raised in situations other than the file (printer)
+        #   not existing so this should probably be tightened up.
+        try:
+            FileNotFoundError
+        except NameError:
+            FileNotFoundError = IOError
+
+        if self.interface == 'USB':
+            try:
+                self._printer = escpos_printer.Usb(self.spool)
+            except FileNotFoundError:
+                raise PrinterNotFound(
+                    'Unable to locate printer "' + self.spool + '".'
+                )
+
+        elif self.interface == 'SERIAL':
+            try:
+                self._printer = escpos_printer.Serial(self.spool)
+            except FileNotFoundError:
+                raise PrinterNotFound(
+                    'Unable to locate printer "' + self.spool + '".'
+                )
+
+        elif self.interface == 'NETWORK':
+            import socket
+            try:
+                self._printer = escpos_printer.Network(self.spool)
+            except socket.error as exc:
+                raise PrinterNotFound(
+                    'Unable to connect to printer: "' + str(exc) + '".'
+                )
+
+        elif self.interface == 'FILE':
+            try:
+                self._printer = escpos_printer.File(self.spool)
+            except FileNotFoundError:
+                raise PrinterNotFound(
+                    'Unable to locate printer "' + self.spool + '".'
+                )
+
         else:
-            # TODO: handle unsupported printer types
-            pass
+            raise PrinerSettingsError(
+                'Interface "' + self.interface + '" unsupported by printer driver.'
+            )
 
     def print_line(self, line):
         self._printer.text(line + "\n")
@@ -154,10 +209,38 @@ class ESCPOSPrinter(object):
         expected = ['code', 'bc', 'width', 'height', 'pos', 'font']
         for key in expected:
             if key not in kwargs:
-                raise Exception("All arguments rquired")
+                raise PrinerArgumentError(
+                    "All arguments required by driver for barcode print")
 
         self._printer.barcode(kwargs['code'], kwargs['bc'], kwargs['width'],
                               kwargs['height'], kwargs['pos'], kwargs['font'])
+
+
+class PrinerSettingsError(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class PrinerArgumentError(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class PrinterAttributeError(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
 
 
 class PrinterNotFound(Exception):
@@ -176,17 +259,3 @@ class PrinterTypeNotSupported(Exception):
 
     def __str__(self):
         return self.value
-
-if __name__ == '__main__':
-    # will use the orthosie driver
-    printer = Printer({'spool': '/dev/null', 'driver': 'orthosie', 'interface': ''})
-    printer = Printer('/dev/null')  # will use the orthosie driver
-    printer = Printer({'spool': '192.168.192.168', 'driver': 'escpos', 'interface': 'network'})
-    kwargs = {'font': 'A', 'align': 'CENTER'}
-    printer.set(**kwargs)
-    printer.print_line('Hello World!')
-    kwargs = {'mode': 'PART'}
-    printer.cut(**kwargs)  # this is a part cut
-    kwargs = {'pin': 2}
-    printer.kick_drawer(**kwargs)  # draw kick using pin 2
-    printer.cut()  # this is a full cut
